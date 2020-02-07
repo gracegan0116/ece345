ca06ab8851c3355259864e4961da35a4b33e6814
Grace Gan <gracegan0116@gmail.com>
2020-02-07 17:35:41 -0500

    fix check_duplicate error

--- assignment_2/checkpass.py
+++ assignment_2/checkpass.py
@@ -2,13 +2,14 @@
 from linked_list import LinkedList
 import sys
 
+
 def checkpass(file, new_password):
     # open the txt file
     password_file = open(file, 'r+')
     passwords = password_file.read().split()
     passwords_table = [LinkedList() for _ in range(62)]
 
-    # storing passwords in passwords_table (hash table)
+    # storing passwords and the reversed_passwords in passwords_table (hash table)
     for password in passwords:
         first_char = password[0]
         if first_char.islower():
@@ -21,16 +22,25 @@ def checkpass(file, new_password):
             index = ord(first_char) - 48
             passwords_table[index].insert_at_end(password[1::])
 
+        last_char = password[-1]
+        if last_char.islower():
+            index = ord(last_char) - 61
+            passwords_table[index].insert_at_end(password[-2::-1])
+        elif last_char.isupper():
+            index = ord(last_char) - 55
+            passwords_table[index].insert_at_end(password[-2::-1])
+        else:
+            index = ord(last_char) - 48
+            passwords_table[index].insert_at_end(password[-2::-1])
+
     # check if new_password is valid
-    # 1. check length
+    # 1. check length (valid length: 6-12)
     length = len(new_password)
-    # check the length of the password which will be 6 to 12 characters long
     if length < 6 or length > 12:
         print("INVALID")
         return
     
     # 2. check alnum
-    # Version1, directly use the python function
     if new_password.isalnum() != True:
         print("INVALID")
         return
@@ -39,19 +49,33 @@ def checkpass(file, new_password):
     first_char = new_password[0]
     if first_char.islower():
         index = ord(first_char) - 61
-        check1 = passwords_table[index].check_duplicate(new_password)
+        check1 = passwords_table[index].check_duplicate(new_password[1::])
     elif first_char.isupper():
         index = ord(first_char) - 55
-        check1 = passwords_table[index].check_duplicate(new_password)
+        check1 = passwords_table[index].check_duplicate(new_password[1::])
     else:
         index = ord(first_char) - 48
-        check1 = passwords_table[index].check_duplicate(new_password)
+        check1 = passwords_table[index].check_duplicate(new_password[1::])
     if check1:
-        # print('duplicate exists')
         print("INVALID")
         return
 
-    # if valid add to file
+    # 4. check reverse duplicate
+    last_char = new_password[-1]
+    if last_char.islower():
+        index = ord(last_char) - 61
+        check2 = passwords_table[index].check_duplicate(new_password[-2::-1])
+    elif last_char.isupper():
+        index = ord(last_char) - 55
+        check2 = passwords_table[index].check_duplicate(new_password[-2::-1])
+    else:
+        index = ord(last_char) - 48
+        check2 = passwords_table[index].check_duplicate(new_password[-2::-1])
+    if check2:
+        print("INVALID")
+        return
+
+    # if valid add to hash table and file
     passwords_table[index].insert_at_end(new_password)
     password_file.write(new_password+"\n")
     print("VALID")
