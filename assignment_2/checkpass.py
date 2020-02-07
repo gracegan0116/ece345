20cba9099b848a9decbacdd51695a91005a8f494
xinchen98 <50330821+xinchen98@users.noreply.github.com>
2020-02-07 01:27:34 -0500

    merge check.py and checkpass.py

--- assignment_2/checkpass.py
+++ assignment_2/checkpass.py
@@ -1,5 +1,5 @@
-from assignment_2.linked_list import LinkedList
-
+from linked_list import LinkedList
+import sys
 
 def checkpass(file, new_password):
     # open the txt file
@@ -21,7 +21,20 @@ def checkpass(file, new_password):
             passwords_table[index].insert_at_end(password[1::])
 
     # check if new_password is valid
-    # 1. check duplicate
+    # 1. check length
+    length = len(new_password)
+    # check the length of the password which will be 6 to 12 characters long
+    if length < 6 or length > 12:
+        print("INVALID")
+        return
+    
+    # 2. check alnum
+    # Version1, directly use the python function
+    if new_password.isalnum() != True:
+        print("INVALID")
+        return
+
+    # 3. check duplicate
     first_char = new_password[0]
     if first_char.islower():
         index = ord(first_char) - 61
@@ -33,14 +46,14 @@ def checkpass(file, new_password):
         index = ord(first_char) - 48
         check1 = passwords_table[index].check_duplicate(new_password)
     if check1:
-        print('duplicate exists')
-    # 2. check alnum
-
-    # 3. check length
-
+        # print('duplicate exists')
+        print("INVALID")
+        return
 
     # if valid add to file
+    passwords_table[index].insert_at_end(new_password)
+    print("VALID")
 
 
 if __name__ == "__main__":
-    checkpass('passwords.txt', 'Fr2C6XNJbA')
+    checkpass(sys.argv[1], sys.argv[2])
